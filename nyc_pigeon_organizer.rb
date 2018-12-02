72e6b5d8acbb372e1d80ec572e4cf4e4a90327ee
Dania Herrera <danherrera0@gmail.com>
2018-12-02 23:23:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,27 @@
+require "pry"
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
-  pigeon_data.each do |color,gender,lives|
-    gender.each do |male, female|
-    names= male.values
-    female_names=female.values
-names.push(female_names.flatten!)
-      pigeon_list={}
-      names.each do |pigeon_name|
-        pigeon_list << pigeon_name
-      pigeon_list[pigeon_name][color]=
-      pigeon_data.each do |color,gender,lives|
-      color
-      pigeon_list[pigeon_name][gender]=
-      pigeon_data.each do |color,gender,lives|
-      gender
-      pigeon_list[pigeon_name][lives]=
-      pigeon_data.each do |color,gender,lives|
-      lives
+  pigeon_list={}
+    data.each do |attributes,details|
+      pigeon_list[:attributes]=attributes
+        binding.pry
+#     gender.each do |male, female|
+#     names= male.values
+#     female_names=female.values
+# names.push(female_names.flatten!)
+#       pigeon_list={}
+#       names.each do |pigeon_name|
+#         pigeon_list << pigeon_name
+#       pigeon_list[pigeon_name][color]=
+#       data.each do |color,gender,lives|
+#       color
+#       pigeon_list[pigeon_name][gender]=
+#       data.each do |color,gender,lives|
+#       gender
+#       pigeon_list[pigeon_name][lives]=
+#       data.each do |color,gender,lives|
+#       lives
+end
 end
